70ad2944a613dfd7451ad43478c8c5561ba512ef
lukaq <82107173+kuc-lukasz@users.noreply.github.com>
2021-07-18 15:15:04 +0200

    main page is done, add responsive to mobile

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const hamburgerBtn = document.querySelector("#hamburger")
+const navMenu = document.querySelector(".nav-menu")
 
 function mobile () {
+    hamburgerBtn.classList.toggle("active");
+    navMenu.classList.toggle("active");
     console.log('dziala')
+    
 }
 
 hamburgerBtn.addEventListener('click', mobile)
